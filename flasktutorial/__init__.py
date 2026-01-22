f0ca4cf2109923451fbd0de4806c1628b5563167
manoj-pun <manojpun840@gmail.com>
2026-01-22 10:07:44 +0545

    learnt about user authentication

--- flasktutorial/__init__.py
+++ flasktutorial/__init__.py
@@ -17,5 +17,13 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+from flasktutorial.models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 from flasktutorial import routes
