64aa0d060dcccc849b240ddffc17cc6a2f0db0a4
manoj-pun <manojpun840@gmail.com>
2026-01-23 09:59:36 +0545

    started user account and profile picture

--- flasktutorial/__init__.py
+++ flasktutorial/__init__.py
@@ -3,19 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 # Create a new Flask web application
 # 'app' is the variable that stores our Flask app object
 # '__name__' tells Flask that this file is the main program
 # Flask uses this to know where to look for templates and static files
 app = Flask(__name__)
-# print(__name__)
 
-app.config["SECRET_KEY"] = 'd70c9ffca4a02b6a78d8185357e6424a'
+app.config["SECRET_KEY"] = "d70c9ffca4a02b6a78d8185357e6424a"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
