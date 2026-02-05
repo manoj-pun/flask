39ffa7c1ee9c0b7de5ea29bfe4b2c959ad5de054
manoj-pun <manojpun840@gmail.com>
2026-02-05 09:15:31 +0545

    fix: AppConfig from Object

--- flasktutorial/__init__.py
+++ flasktutorial/__init__.py
@@ -11,27 +11,36 @@ from flasktutorial.config import Config
 # 'app' is the variable that stores our Flask app object
 # '__name__' tells Flask that this file is the main program
 # Flask uses this to know where to look for templates and static files
-app = Flask(__name__)
-app.config.from_object(Config)
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
 
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = "users.login"
 login_manager.login_message_category = "info"
+mail = Mail()
 
-mail = Mail(app)
-
-from flasktutorial.models import User
 
 # print("MAIL USER:", app.config["MAIL_USERNAME"])
 # print("MAIL PASS LENGTH:", len(app.config["MAIL_PASSWORD"]) if app.config["MAIL_PASSWORD"] else None)
 
-from flasktutorial.users.routes import users
-from flasktutorial.posts.routes import posts
-from flasktutorial.main.routes import main
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flasktutorial.users.routes import users
+    from flasktutorial.posts.routes import posts
+    from flasktutorial.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
+    
--- flasktutorial/users/utils.py
+++ flasktutorial/users/utils.py
@@ -1,15 +1,15 @@
 import os
 import secrets
 from PIL import Image
-from flask import url_for
+from flask import url_for, current_app
 from flask_mail import Message
-from flasktutorial import app, mail
+from flasktutorial import mail
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, "static/profile_pics",picture_fn)
+    picture_path = os.path.join(current_app.root_path, "static/profile_pics",picture_fn)
 
     output_size = (125,125)
     i = Image.open(form_picture)
@@ -20,7 +20,7 @@ def save_picture(form_picture):
 
 def send_reset_email(user):
     token = user.get_reset_token()
-    msg = Message("Password Reset Request",sender=app.config["MAIL_USERNAME"],recipients=[user.email])
+    msg = Message("Password Reset Request",sender=current_app.config["MAIL_USERNAME"],recipients=[user.email])
 
     msg.body = f'''To reset your password, visit the following link:
 {url_for('users.reset_token', token=token, _external=True)}
